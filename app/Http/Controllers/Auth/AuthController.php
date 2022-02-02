2086bbe7f48478dbcde72b501059c5ce62d5b9d5
widesnow-t <89765385+widesnow-t@users.noreply.github.com>
2022-02-02 21:56:27 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,9 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
     /** 
      * @return View
     */
@@ -26,11 +31,38 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
+        //1,アカウントがロックされていたら弾く
+        $user = User::where('email', '=', $credentials['email'])->first();
+
+        if(!is_null($user)){
+            if ($user->locked_flg === 1){
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされてます。',
+                ]);
+            }
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-
+                //２、成功したらエラーアカウントを０にする
+                if ($user->error_count > 0){
+                $user->error_count = 0;
+                $user->save();
+                }
             return redirect()->route('home')->with('success', 'ログイン成功しました!');
         }
+            //3.ログインが失敗したらエラーカウントを１増やす
+            $user->error_count = $user->error_count + 1;
+            //4,エラーカウントが６以上の場合はアカウントロックする
+            if ($user->error_count > 5) {
+                $user->locked_flg = 1;
+                $user->save();
+
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。解除したい場合は運営者に連絡ください。',
+                ]);
+            }
+            $user->save();
+        
+    }
 
         return back()->withErrors([
             'danger' => 'メールアドレスかパスワードが間違ってます。',
